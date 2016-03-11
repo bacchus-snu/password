a8fd4702e44c4a33175478d474f146bd851cd016
Hyeon Kim <simnalamburt@gmail.com>
2016-03-11 23:00:59 +0900

    React 쓰도록 수정

--- src/main.js
+++ src/main.js
@@ -7,4 +7,9 @@ import secret from './secret.md'
 import 'github-markdown-css/github-markdown.css'
 import './main.styl'
 
-document.getElementById('target').innerHTML = marked(secret);
+const html = {__html: marked(secret)};
+
+ReactDOM.render(
+  <div className='markdown-body' dangerouslySetInnerHTML={html}/>,
+  document.getElementById('target')
+);
