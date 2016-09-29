ae487948d2eabb6b47429a13b305cad73d186998
VBChunguk <chwo9843@gmail.com>
2016-09-29 18:45:59 +0900

    비밀번호 추가 전에 마스터 비밀번호를 확인함

--- backend/src/crypto.rb
+++ backend/src/crypto.rb
@@ -11,19 +11,17 @@ module CryptoHelper
       SecretBox.key_bytes # digest_size
     )
   end
-  def CryptoHelper.encrypt(password, plain, salt64=nil)
-    salt = if salt64
-      Base64.decode64(salt64)
-    else
-      RbNaCl::Random.random_bytes(PasswordHash::SCrypt::SALTBYTES)
-    end
+  def CryptoHelper.generate_salt()
+    RbNaCl::Random.random_bytes(PasswordHash::SCrypt::SALTBYTES)
+  end
+  def CryptoHelper.encrypt(password, plain, salt64)
+    salt = Base64.decode64(salt64)
     digest = CryptoHelper.key(password, salt)
     secret_box = SecretBox.new(digest)
     nonce = RbNaCl::Random.random_bytes(secret_box.nonce_bytes)
     ciphertext = secret_box.box(nonce, plain)
 
     {
-      :salt64 => Base64.encode64(salt),
       :nonce64 => Base64.encode64(nonce),
       :ciphertext64 => Base64.encode64(ciphertext)
     }
--- backend/src/keygen.rb
+++ backend/src/keygen.rb
@@ -5,24 +5,60 @@ if not Dir.exist?('password')
   puts 'Creating directory password/'
   Dir.mkdir('password')
 end
-salt64 = if File.exist?('password/salt')
+salt64, crypt_nonce64, crypt_master64 = if File.exist?('password/salt')
   File.open('password/salt') do |file|
-    file.readline
+    salt64 = file.readline
+    crypt_nonce64 = file.readline
+    crypt_master64 = file.readline
+    [salt64, crypt_nonce64, crypt_master64]
   end
 else
-  nil
+  puts 'Initializing the password storage for the first time.'
+  salt = CryptoHelper.generate_salt()
+  print 'Master password: '
+  `stty -echo`
+  master = gets.chomp
+  `stty echo`
+  puts ''
+  salt64 = Base64.encode64(salt)
+  crypt_result = CryptoHelper.encrypt(master, salt, salt64)
+  crypt_master64 = crypt_result[:ciphertext64].gsub(/\n/, '')
+  crypt_nonce64 = crypt_result[:nonce64].gsub(/\n/, '')
+  File.open('password/salt', 'w') do |file|
+    file.puts(salt64)
+    file.puts(crypt_nonce64)
+    file.puts(crypt_master64)
+  end
+  [salt64, crypt_nonce64, crypt_master64]
 end
 puts 'Will create password file in password/'
+
+print 'Verify master password: '
+`stty -echo`
+master = gets.chomp
+`stty echo`
+puts ''
+
+begin
+  salt = Base64.decode64(salt64)
+  secret_box = RbNaCl::SecretBox::new(CryptoHelper.key(master, salt))
+  dec_salt = secret_box.open(
+    Base64.decode64(crypt_nonce64),
+    Base64.decode64(crypt_master64)
+  )
+  raise 'Salt does not match' unless RbNaCl::Util.verify32(
+    RbNaCl::Hash.sha256(dec_salt),
+    RbNaCl::Hash.sha256(salt)
+  )
+rescue
+  puts 'Password verification failed.'
+  exit 1
+end
 
 print 'Password ID: '
 id = gets.chomp
 print 'Password description: '
 description = gets.chomp
-print 'Master password: '
-`stty -echo`
-master = gets.chomp
-`stty echo`
-puts ''
 print 'Plain password to encrypt: '
 `stty -echo`
 plain = gets.chomp
@@ -30,12 +66,6 @@ plain = gets.chomp
 puts ''
 
 data = CryptoHelper.encrypt(master, plain, salt64)
-
-if not File.exist?('password/salt')
-  File.open('password/salt', 'w') do |file|
-    file << data[:salt64]
-  end
-end
 
 File.open("password/#{id}.p", 'w') do |file|
   file.puts(Base64.encode64(description))
