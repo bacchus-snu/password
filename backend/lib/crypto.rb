23e1fe4d53f0ee1f8697bfa35c93ab45672b436d
VBChunguk <chwo9843@gmail.com>
2016-10-10 16:17:14 +0900

    백엔드 코드에 주석 닮

--- backend/lib/crypto.rb
+++ backend/lib/crypto.rb
@@ -4,6 +4,7 @@ require 'base64'
 include RbNaCl
 
 module CryptoHelper
+  # 비밀번호와 salt로 key를 만든다
   def CryptoHelper.key(password, salt)
     PasswordHash.scrypt(
       password, salt,
@@ -11,9 +12,11 @@ module CryptoHelper
       SecretBox.key_bytes # digest_size
     )
   end
+  # salt에 필요한 길이의 랜덤 바이트를 생성한다
   def CryptoHelper.generate_salt()
     RbNaCl::Random.random_bytes(PasswordHash::SCrypt::SALTBYTES)
   end
+  # 평문, 비밀번호, base64 인코딩된 salt로 평문을 암호화한다
   def CryptoHelper.encrypt(password, plain, salt64)
     salt = Base64.decode64(salt64)
     digest = CryptoHelper.key(password, salt)
